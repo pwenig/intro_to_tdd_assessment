2f31b38307f70e479d0e0bac6f933830d36bec56
Paul Wenig <pwenig@gmail.com>
2014-03-21 09:14:25 -0600

    second test passed. returns false when number is not prime

--- spec/prime_calc_spec.rb
+++ spec/prime_calc_spec.rb
@@ -5,5 +5,10 @@ it "returns true when number is prime" do
   actual = true
   expected = Prime.prime?(3)
   expect(actual).to eq expected
+end
+  it "returns false when number is not prime" do
+    actual = false
+    expected = Prime.prime?(6)
+    expect(actual).to eq expected
   end
 end
